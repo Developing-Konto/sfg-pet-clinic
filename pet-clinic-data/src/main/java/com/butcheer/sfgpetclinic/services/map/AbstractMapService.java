71fb42f1310db9643dd7390adf365a8fd809739b
Butcheer <developing.konto@gmail.com>
2019-03-01 14:29:00 +0100

    Impl map based Services. Closes #6.

---
+++ pet-clinic-data/src/main/java/com/butcheer/sfgpetclinic/services/map/AbstractMapService.java
@@ -0,0 +1,36 @@
+package com.butcheer.sfgpetclinic.services.map;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Butcheer on 2019-03-01 13:10
+ */
+public abstract class AbstractMapService<T, ID> {
+   protected Map<ID, T> map = new HashMap<>();
+
+   Set<T> findAll() {
+      return new HashSet<>(map.values());
+   }
+
+   T findById(ID id) {
+      return map.get(id);
+   }
+
+   T save(ID id, T object) {
+      map.put(id, object);
+      return object;
+   }
+
+   void deleteById(ID id) {
+      map.remove(id);
+   }
+
+   void delete(T object) {
+      map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
+   }
+
+
+}
---
+++ pet-clinic-data/src/main/java/com/butcheer/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,37 @@
+package com.butcheer.sfgpetclinic.services.map;
+
+import com.butcheer.sfgpetclinic.model.Vet;
+import com.butcheer.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+/**
+ * Created by Butcheer on 2019-03-01 14:25
+ */
+public class VetServiceMap extends AbstractMapService<Vet, Long>
+   implements CrudService<Vet, Long> {
+   @Override
+   public Set<Vet> findAll() {
+      return super.findAll();
+   }
+
+   @Override
+   public void deleteById(Long id) {
+      super.deleteById(id);
+   }
+
+   @Override
+   public void delete(Vet object) {
+      super.delete(object);
+   }
+
+   @Override
+   public Vet save(Vet object) {
+      return super.save(object.getId(), object);
+   }
+
+   @Override
+   public Vet findById(Long id) {
+      return super.findById(id);
+   }
+}
