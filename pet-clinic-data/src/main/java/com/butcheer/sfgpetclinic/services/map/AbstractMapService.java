f919635b2187e6297b811ea5c9eed5514878d044
Butcheer <developing.konto@gmail.com>
2019-03-08 15:02:01 +0100

    Update Map Impl to manage setting of Id Property. closes #23.

--- pet-clinic-data/src/main/java/com/butcheer/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/butcheer/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,15 +1,15 @@
 package com.butcheer.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.butcheer.sfgpetclinic.model.BaseEntity;
+
+import java.util.*;
 
 /**
  * Created by Butcheer on 2019-03-01 13:10
  */
-public abstract class AbstractMapService<T, ID> {
-   protected Map<ID, T> map = new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {// ?extends Long?
+
+   protected Map< Long, T> map = new HashMap<>();
 
    Set<T> findAll() {
       return new HashSet<>(map.values());
@@ -19,8 +19,14 @@ public abstract class AbstractMapService<T, ID> {
       return map.get(id);
    }
 
-   T save(ID id, T object) {
-      map.put(id, object);
+   T save(T object) {
+      if (object == null) {
+         throw new RuntimeException("Object cannot be null");
+      }
+      if (object.getId() == null) {
+         object.setId(getNextId());
+      }
+      map.put(object.getId(), object);
       return object;
    }
 
@@ -32,5 +38,10 @@ public abstract class AbstractMapService<T, ID> {
       map.entrySet().removeIf(idtEntry -> idtEntry.getValue().equals(object));
    }
 
+
+   private  Long getNextId() {
+
+      return map.isEmpty() ? 1L : Collections.max(map.keySet()) + 1L;
+   }
 
 }
