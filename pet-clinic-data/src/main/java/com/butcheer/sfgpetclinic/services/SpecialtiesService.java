3d176e1ba2b5109334cb4a6ea35e272dd8acbf1d
Butcheer <developing.konto@gmail.com>
2019-03-29 13:18:27 +0100

    added  Specialties Map Service Impl. closes #31

---
+++ pet-clinic-data/src/main/java/com/butcheer/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,9 @@
+package com.butcheer.sfgpetclinic.services;
+
+import com.butcheer.sfgpetclinic.model.Speciality;
+
+/**
+ * Created by Butcheer on 2019-03-29 13:08
+ */
+public interface SpecialtiesService extends CrudService<Speciality, Long > {
+}
---
+++ pet-clinic-data/src/main/java/com/butcheer/sfgpetclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,39 @@
+package com.butcheer.sfgpetclinic.services.map;
+
+import com.butcheer.sfgpetclinic.model.Speciality;
+import com.butcheer.sfgpetclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created by Butcheer on 2019-03-29 13:10
+ */
+@Service
+public class SpecialityMapService extends AbstractMapService<Speciality, Long>
+   implements SpecialtiesService {
+   @Override
+   public Set<Speciality> findAll() {
+      return super.findAll();
+   }
+
+   @Override
+   public void deleteById(Long id) {
+      super.deleteById(id);
+   }
+
+   @Override
+   public void delete(Speciality object) {
+      super.delete(object);
+   }
+
+   @Override
+   public Speciality save(Speciality object) {
+      return super.save(object);
+   }
+
+   @Override
+   public Speciality findById(Long id) {
+      return super.findById(id);
+   }
+}
